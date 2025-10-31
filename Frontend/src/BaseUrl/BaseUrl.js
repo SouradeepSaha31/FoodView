29c1cf4cc77739c9ddd7f9cfbbe8375851459aef
Souradeep Saha <souradeepsaha314@gmail.com>
2025-10-31 14:44:23 +0530

    run build command

--- Frontend/src/BaseUrl/BaseUrl.js
+++ Frontend/src/BaseUrl/BaseUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://foodview-backend-46jq.onrender.com',
     withCredentials: true
 });
 
